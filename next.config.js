8f06848f0232fe4b5415750e06359ff0dd98b1db
luvvers <94322258+luvvers@users.noreply.github.com>
2023-09-03 12:09:40 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    IP_GEOLOCATION_API_KEY: process.env.IP_GEOLOCATION_API_KEY,
+    IP_GEOLOCATION_API_KEY: process.env.4e145d3009f3481786b08ff7c64fb493,
   },
 };
 
